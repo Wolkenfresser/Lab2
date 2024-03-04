141c3aedb490d635243e4cb67ef5092a8dc93fe0
Nik <136103496+Wolkenfresser@users.noreply.github.com>
2024-03-04 10:10:28 +0300

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,32 @@
+///Дано 2 файла вещественных чисел а.txt и b.txt. Найти произведение отрицательных элементов двух файлов
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    ifstream file1("a.txt");
+    ifstream file2("b.txt");
+
+    double num1, num2;
+    double product = 1.0;
+
+    if (file1.is_open() && file2.is_open()) {
+        while (file1 >> num1 && file2 >> num2) {
+            if (num1 < 0 && num2 < 0) {
+                product *= num1 * num2;
+            }
+        }
+
+        file1.close();
+        file2.close();
+
+        cout << "Произведение отрицательных элементов двух файлов: " << product << endl;
+    }
+    else {
+        cout << "Не удалось открыть один из файлов." << endl;
+    }
+
+    return 0;
+}
