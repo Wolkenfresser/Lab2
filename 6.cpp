d667d428eac355f5c8b4777b86187b2a6185b3c5
Nik <136103496+Wolkenfresser@users.noreply.github.com>
2024-03-04 10:59:16 +0300

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -1,40 +1,30 @@
-///Дан файл f, компоненты которого являются целыми числами. Получить в файле g все компоненты файла f, являющиеся точными квадратами.
+///Дан файл f, компоненты которого являются целыми числами. Получить в файле g все компоненты файла f являющиеся чётными числами.
 #include <iostream>
 #include <fstream>
-#include <vector>
-using namespace std;
-
-bool isPerfectSquare(int number) {
-    int kor = sqrt(number);
-    return kor * kor == number;
-}
 
 int main() {
-    setlocale(LC_ALL, "RU");
-    ofstream File("text.txt");
-    if (File.is_open())
-    {
-        File << "49 25 65 12 16 81 56 73 100 36 55" << endl;
-    }
-    
-    File.close();
+    std::ifstream inputFile("f.txt");
+    std::ofstream outputFile("g.txt");
 
-    ifstream inputFile("text.txt");
-    ofstream outputFile("kvadr.txt");
+    if (!inputFile) {
+        std::cerr << "Не удается открыть файл f.txt\n";
+        return 1;
+    }
+
+    if (!outputFile) {
+        std::cerr << "Не удается создать файл g.txt\n";
+        return 1;
+    }
+
     int number;
-
-    if (inputFile.is_open() && outputFile.is_open()) {
-        while (inputFile >> number) {
-            if (isPerfectSquare(number)) {
-                outputFile << number << endl;
-            }
+    while (inputFile >> number) {
+        if (number % 2 == 0) {
+            outputFile << number << std::endl;
         }
-        inputFile.close();
-        outputFile.close();
     }
-    else {
-        cout << "Не удалось открыть файл" << endl;
-    }
+
+    inputFile.close();
+    outputFile.close();
 
     return 0;
 }
